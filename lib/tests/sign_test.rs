5c9d191290c54a6b824b771aba8bab205351ec1f
Erreiip <lolpierre154@gmail.com>
2026-04-25 10:12:29 +0200

    Resolve "Add test for signature"

---
+++ lib/tests/sign_test.rs
@@ -0,0 +1,18 @@
+#[cfg(test)]
+mod tests {
+    use lib::sign::{signer::Signer, signer_dilithium2::SignerDilithium2, verifier::Verifier};
+
+
+    #[test]
+    fn sign_test() {
+
+        let signer = SignerDilithium2::create_sign();
+        let verifier = SignerDilithium2::create_verifier();
+
+        let hash: Vec<u8> = vec![1,2];
+        let sfile_size = 10;
+
+        let sig = signer.sign(hash.clone(), sfile_size);
+        assert_eq!(verifier.verify(sig, hash.clone(), sfile_size), true)
+    }
+}
